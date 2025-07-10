0992b096f58cfb61d6c83717e3f7e4055cdc3513
Charleskojomark <markcharleskojo@gmail.com>
2025-07-10 05:21:28 +0100

    next

--- src/controllers/townHallController.js
+++ src/controllers/townHallController.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const logger = require('../utils/logger');
-
+// 
 const prisma = new PrismaClient();
 
 const createTownhall = async (req, res, next) => {
